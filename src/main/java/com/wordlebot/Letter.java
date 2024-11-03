ca0fbe07013bc19954717bd99f77159a43fbe073
cat <sfallon42@gmail.com>
2024-11-03 17:48:33 -0500

    Implemented methods in Letter class

--- src/main/java/com/wordlebot/Letter.java
+++ src/main/java/com/wordlebot/Letter.java
@@ -2,16 +2,78 @@ package com.wordlebot;
 /*
  * A class that keeps count of many different letters and how many times 
  * the letter appears in each possible position in wordle
+ * 
+ * int[] counts stores the amount of times the letter appears in that position in the wordle word
+ * for example, if the letter appears in the first position of a wordle word 50 times,
+ * then count[0] = 50
  */
 public class Letter {
     public char letter;
-    int[] counts;
+    int[] counts; //stores the amount of times the letter appears in that position in the wordle word
 
     public Letter(char letter) {
         this.letter = letter;
         for (int i = 0; i < 5; i++) {
-            counts[i] = 0;
+            this.counts[i] = 0;
         }
     }
-//TODO: Finish all the other functions for Letter class
+
+    /** 
+     * Returns the character the Letter is associated with
+     * @return char
+     */
+    public char getChar() {
+        return this.letter;
+    }
+
+    /** 
+     * sets the character the Letter is associated with
+     * @param letter new char to be associated with the letter
+     */
+    public void setChar(char letter) {
+        this.letter = letter;
+    }
+
+    /** 
+     * returns each count associated with the Letter
+     * @return int[] the array of all countes for the letter's positions
+     */
+    public int[] getCounts() {
+        return this.counts;
+    }
+
+    /** 
+     * sets the arraylist of counts associated with the letter
+     * @param counts
+     */
+    public void setCounts(int[] counts) {
+        this.counts = counts;
+    }
+
+    /** 
+     * returns the count associated with the given position.
+     * The index should be the position or tile in which to find the count
+     * @param index which count to return
+     * @return int the count 
+     */
+    public int getCountFromIndex(int index) {
+        return this.counts[index];
+    }
+
+    /** 
+     * sets the count in counts to a new given int
+     * @param index which index of count to set
+     * @param count what to set the new count to
+     */
+    public void setCountFromIndex(int index, int count) {
+        this.counts[index] = count;
+    }
+
+    /*
+     * Increases the count found with index by 1
+     * @param index the index of the count to increment
+     */
+    public void incrementCount(int index) {
+        this.counts[index] = this.counts[index] + 1;
+    }
 }
--- src/main/java/com/wordlebot/WordleBot.java
+++ src/main/java/com/wordlebot/WordleBot.java
@@ -1,28 +1,47 @@
 package com.wordlebot;
 
 import java.util.ArrayList;
-import java.util.Set;
 
 public class WordleBot {
+
+    /*
+     * A method that iterates through an ArrayList of Letters to
+     * find one that has the given character
+     * @param ch the character that represents the Letter to be found
+     * @param letters the ArrayList of Letters to search through
+     * @return Letter the letter associated with the given character
+     */
+    public static Letter findLetter(char ch, ArrayList<Letter> letters) {
+        Letter le = new Letter('1')
+        for (int i = 0; i < letters.size(); i++) {
+            if (letters.get(i).getChar() == ch) {
+                le = letters.get(i);
+            }
+        }
+        return le;
+    }
+
 
     public static void analyzeGuesses(ArrayList<String> answers) {
         
         ArrayList<Character> lettersArrList = WordleGame.createCharArray();
         ArrayList<Letter> letters = new ArrayList<Letter>();
-        //Set<Letter> letters = new Set<Letter>();
 
         for (int i = 0; i < lettersArrList.size(); i++) {
             letters.add(new Letter(lettersArrList.get(i)));
         }
+        ArrayList<Letter> maxes = new ArrayList<Letter>(5); 
+        //should store the letter that appears most in each position
         for (int j = 0; j < answers.size(); j++) {
             String word = answers.get(j);
             for (int k = 0; k < word.length(); k++) {
                 char ch = word.charAt(k);
-                Letter l = letters.get(letters.indexOf(ch));
-                //TODO: this isnt working I think I need to make a function 
-                //that searches an ArrayList of letters and returns that letter
-                //or maybe im too married to arraylists in this project
-                //there must be some library that does that already
+                
+                Letter l = findLetter(ch, letters);
+                l.incrementCount(k);
+                if (l.getCountFromIndex(k) > maxes.get(k).getCountFromIndex(k)) {
+                    maxes.set(k, l); 
+                }
 
             }
         }
